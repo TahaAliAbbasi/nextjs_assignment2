2fe88263714a6bb450a093e2c3bc655bf6533ac8
TahaAliAbbasi <163841832+TahaAliAbbasi@users.noreply.github.com>
2024-10-17 12:00:33 -0700

    assignment2

--- app/components/Countries.tsx
+++ app/components/Countries.tsx
@@ -1,6 +1,6 @@
 import { ReactElement, JSXElementConstructor, ReactNode, ReactPortal, AwaitedReactNode } from "react";
 
-export default function Countries(props: { name: string | number | bigint | boolean | ReactElement<any, string | JSXElementConstructor<any>> | Iterable<ReactNode> | ReactPortal | Promise<AwaitedReactNode> | null | undefined; population: string | number | bigint | boolean | ReactElement<any, string | JSXElementConstructor<any>> | Iterable<ReactNode> | ReactPortal | Promise<AwaitedReactNode> | null | undefined; capital_city: string | number | bigint | boolean | ReactElement<any, string | JSXElementConstructor<any>> | Iterable<ReactNode> | ReactPortal | Promise<AwaitedReactNode> | null | undefined; }){
+export default function Countries(props: { name: string ; population: string | number ; capital_city: string ; }){
     
 return(
     <>
